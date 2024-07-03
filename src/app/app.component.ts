a44116e4f4484d9105a9a85f842f41f42f934773
nazuaga <nazuaga.globallogic@bancochile.cl>
2024-07-03 01:02:49 -0300

    feat: poke list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'seminario-angular-2024';
+  title = 'PokeFinder';
+  year = 2024;
 }
