066b24c458a5037431815bc41dea6b5a4fda0709
nazuaga <nazuaga.globallogic@bancochile.cl>
2024-07-05 01:22:08 -0300

    feat: se agregan rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonScreenComponent } from './pokemon-screen/pokemon-screen.component';
+import { ShopScreenComponent } from './shop-screen/shop-screen.component';
+import { PokemonStorageScreenComponent } from './pokemon-storage-screen/pokemon-storage-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: PokemonScreenComponent,
+  },
+  { path: 'shop', component: ShopScreenComponent },
+  { path: 'pokemon-storage', component: PokemonStorageScreenComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
